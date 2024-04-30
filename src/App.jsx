22d4a3d25074764eeef1f18bafda12f96d8bc293
anishmendes <anishmainali31@gmail.com>
2024-04-30 12:54:51 +0545

    Navbar setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,34 @@
 
 
-const App = () => {
+
+import './App.css';
+import Navbar from './Components/Navbar/Navbar'
+import {  Routes, Route, Navigate} from 'react-router-dom';
+import Home from './Components/Pages/Home';
+import Admission from './Components/Pages/Admission'
+import Faculty from './Components/Pages/Faculty'
+import Eca from './Components/Pages/Eca'
+import SignUp from './Components/Pages/Signup'
+import Notice from './Components/Pages/Notice';
+
+function App() {
   return (
-    <div>
-      Hello World 
+    <>
+     <div className='w-full h-100vh overflow-hidde'>
+      <Navbar/>
+    <Routes>
+        <Route path='/' exact element={<Home />} />
+        <Route path="/Admission" element={<Admission />} />
+        <Route path='/Faculty' element={<Faculty />} />
+        <Route path='/Eca' element={<Eca />} />
+        <Route path='/Signup' element={<SignUp />} />
+        <Route path='/Notice' element={<Notice />} />
+        <Route path='*' element={<Navigate to='/' />} />
+    </Routes>
     </div>
-  )
+    </>
+  
+  );
 }
 
-export default App
+export default App;
