1f675b963558047f8309a9fa800d273eb0e41f40
anishmendes <anishmainali31@gmail.com>
2024-05-01 11:11:29 +0545

    almost done faculty section

--- src/components/Pages/Faculty.jsx
+++ src/components/Pages/Faculty.jsx
@@ -25,7 +25,8 @@ const Faculty = () => {
 <span className='text-blue-800 font-bold text-2xl'>Our Team</span>
       <div className="h-[3px] w-[80px] bg-blue-800  ml-[230px] mt-[-18px]"></div>
 
-      {/* image halanw parni item is down below just given background and sizing  */}
+
+{/* image halanw parni item is down below just given background and sizing  */}
       <div className=" h-[400px] w-[1200px] bg-current ml-[90px] mt-[50px] "></div>
    <div className='  ml-[120px] mt-[50px]'>
   <p className='flex-col text-black  w-[1200px]   pt-8'>
@@ -34,8 +35,7 @@ const Faculty = () => {
   <p className='flex-col text-black  w-[1200px]   pt-10'>
   Whether in the classroom, on the sports field, or in the community, our team members exemplify dedication, creativity, and compassion. Together, we celebrate diversity, foster inclusivity, and cultivate a culture of respect and teamwork. With each member bringing their unique skills and perspectives to the table, our team is stronger and more resilient, empowering each other and our students to reach new heights of achievement and fulfillment.
   </p>
-
-  </div>
+   </div>
 
 
 </div>
@@ -46,3 +46,38 @@ const Faculty = () => {
   <div className="h-[3px] w-[80px] bg-white mr-[-120px] ml-[150px]"></div>
 <span className='text-white font-bold ml-[160px] text-2xl'>FOUNDERS</span>
       <div className="h-[3px] w-[80px] bg-white  ml-[30px] "></div>
+
+
+      <p className='flex items-center justify-center font-bold text-xl text-white mt-[100px] mr-[300px]'>Antarikshya School was founded by Mr. Ram Shrestha and Ms. Sita Pathak in 1998</p>
+       
+
+      <div className="flex justify-between space-x-[150px] mr-[300px]">
+  <div className="w-[200px] h-[200px] rounded-full bg-gray-300"></div>
+  <div className="w-[200px] h-[200px] rounded-full bg-gray-300"></div>
+</div>
+
+<div className="flex justify-between space-x-[150px] pt-8 mr-[300px]">
+
+  <div className='w-[352px] h-[98px] bg-gray-300 mr-[80px]'></div>
+  <div className='w-[352px] h-[98px] bg-gray-300 mr-[80px]'></div>
+</div>
+
+
+
+
+
+  </div>
+
+  
+
+</div>
+
+
+
+
+
+    </div>
+  )
+}
+
+export default Faculty
