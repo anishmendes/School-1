e3c348af1dd58895d70d397c57173e3eb112920e
anishmendes <anishmainali31@gmail.com>
2024-05-02 11:31:12 +0545

    imported all necessary components

--- src/components/Pages/Notice.jsx
+++ src/components/Pages/Notice.jsx
@@ -1,1 +1,8 @@
 // this is the facilities section 
+import React from 'react'
+import bus from '../../assets/bus.png'
+import Library1 from '../../assets/Library1.png'
+import Library2 from '../../assets/Library2.png'
+import canteen from '../../assets/canteen.png'
+import Lab1 from "../../assets/Lab1.png"
+import Lab2 from "../../assets/Lab2.png" 
