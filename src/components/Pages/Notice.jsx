d95cab5c462c1ef1c01a9aa0672a3e0a84db4d3d
anishmendes <anishmainali31@gmail.com>
2024-05-02 11:32:23 +0545

    Transportation section done

--- src/components/Pages/Notice.jsx
+++ src/components/Pages/Notice.jsx
@@ -6,3 +6,22 @@ import Library2 from '../../assets/Library2.png'
 import canteen from '../../assets/canteen.png'
 import Lab1 from "../../assets/Lab1.png"
 import Lab2 from "../../assets/Lab2.png" 
+
+const Facilities = () => {
+  return (
+    <div className='container mx-auto pt-10'>
+       <span className='text-blue-800 font-bold text-2xl'>Our  Facilities</span>
+      <div className="h-[3px] w-[80px] bg-blue-900  ml-[230px] mt-[-18px]"></div>
+
+      <p className='flex-col text-blue-900 w-[400px] font-bold pt-12 text-2xl '>Transportion</p>
+
+      <div className='pt-12 flex gap-[100px] justify-center '>
+      <img className='w-[548px] h-[524px] max-sm:w-[500px] rounded-lg py-2' src={bus} alt="" />
+      <img className='w-[548px] max-sm:w-[500px] rounded-lg py-2' src={bus} alt="" />
+      </div>
+
+      </div>
+  )
+}
+
+export default Facilities
